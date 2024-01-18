54edf610af4ba747f529407957b70bb7b2ba0362
Thomas Nielsen <94662330+ThomasNielsen55@users.noreply.github.com>
2024-01-17 21:39:28 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventure2
+{
+    internal class RollSim
+    {
+        private int numRolls;
+        public RollSim(int numRolls) 
+        {
+            this.numRolls = numRolls;
+        }
+
+        public void rollEm()
+        {
+            // do stuff. determine how many rolls occurred for each number.
+            // divide the amount of rolls by numRolls. keep it a whole number. print
+
+            // make variables
+
+            Random random = new Random();
+
+            int[] nums = new int[11];
+
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                int die1 = random.Next(1, 7);
+                int die2 = random.Next(1, 7);
+                int comboRoll = die2 + die1;
+
+                nums[comboRoll - 2]++;
+
+            }
+
+            for (int i = 0; i < 11; i++)
+            {
+                string outputAst = "";
+                double numAst = 100 * (double)nums[i] / numRolls;
+                for (int j = 0; j < numAst; j++)
+                {
+                    outputAst += "*";
+                }
+
+                System.Console.WriteLine((i + 2) + ": " + outputAst);
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.ComponentModel;
+using System.Runtime.Intrinsics.X86;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Threading.Tasks;
+using adventure2;
+
+internal class Program
+{
+
+    //  Write a .NET console application using C# that simulates the rolling of two 6-sided dice. Use an
+    //  Array to keep track of the number of times that each combination is thrown.In other words,
+    //  keep track of how many times the combination of the two simulated dice is 2, how many times
+    //  the combination is 3, and so on, all the way up through 12.
+    //  Allow the user to choose how many times the “dice” will be thrown.Then, once you have the
+    //  number of rolls, pass that number to a second class that has a method that simulates the roll of
+    //  the dice for the number of times that the user specified.That method in the second class should
+    //  return the array containing the results.In the first class, use that array to print a histogram(using
+    //  the* character) that shows the total percentage each number was rolled. Each * will represent
+    //  1% of the total rolls.
+    private static void Main(string[] args)
+    {
+        int numRolls = 0;
+        System.Console.WriteLine("Welcome to dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate? ");
+
+        numRolls = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Each \"*\" represents 1 % of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + numRolls);
+
+        RollSim mySim = new RollSim(numRolls);
+
+
+        mySim.rollEm();
+
+    
+    }
+}
